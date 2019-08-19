42e5d4c72fd3a905aa9aa2e23d9dbf60c13e68cc
Zhanarys Sairanbekov <arsik0153@gmail.com>
2019-08-19 19:25:04 +0600

    Header Form state control add

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,24 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Form.css';
 
-const Form = () => {
-  return (
-    <div className="header__form">
-      <h4>Тіркелу</h4>
-      <form>
-        <label>Аты-жөніңіз</label>
-        <div className="header__form-group">
-          <input type="text" placeholder="Аты" required/>
-          <input type="text" placeholder="Тегі" required/>
-        </div>
-        <label>E-mail</label>
-        <input type="email" placeholder="example@example.com" required/>
-        <label>Құпия сөз</label>
-        <input type="password" placeholder="" required/>
-        <button className="header__form_submit-btn">Тіркелу</button>
-      </form>
-    </div>
-  )
+class Form extends Component{
+
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  render(){
+    return (
+      <div className="header__form">
+        <h4>Тіркелу</h4>
+        <form onSubmit={this.handleSubmit}>
+          <label>Аты-жөніңіз</label>
+          <div className="header__form-group">
+            <input 
+              type="text" 
+              placeholder="Аты"
+              id="firstName"
+              onChange={this.handleChange}
+              required
+            />
+            <input 
+              type="text" 
+              placeholder="Тегі" 
+              id="lastName"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <label>E-mail</label>
+          <input 
+            type="email" 
+            placeholder="example@example.com"
+            id="email"
+            onChange={this.handleChange}
+            required
+          />
+          <label>Құпия сөз</label>
+          <input 
+            type="password" 
+            placeholder="" 
+            id="password"
+            onChange={this.handleChange}
+            required
+          />
+          <button className="header__form_submit-btn">Тіркелу</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default Form
+export default Form;
